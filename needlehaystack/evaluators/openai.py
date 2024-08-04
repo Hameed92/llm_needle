47d070d51e8c63ca4055f329d152b27da68b70a9
Abdulhamed Alothaimen <aalothaimen@sdaia.gov.sa>
2024-08-04 08:08:21 +0300

    added claude backup in evaluation

--- needlehaystack/evaluators/openai.py
+++ needlehaystack/evaluators/openai.py
@@ -4,7 +4,7 @@ from .evaluator import Evaluator
 import re
 from langchain.evaluation import load_evaluator
 from langchain_community.chat_models import ChatOpenAI
-from langchain_anthropic import ChatAnthropic
+import anthropic
 import requests
 import time
 
@@ -12,10 +12,10 @@ class OpenAIEvaluator(Evaluator):
     DEFAULT_MODEL_KWARGS: dict = dict(temperature=0)
     CRITERIA = {"accuracy": """
                 Score 1: The answer is completely unrelated to the reference.
-                Score 3: The answer has minor relevance but does not align with the reference.
-                Score 5: The answer has moderate relevance but contains inaccuracies.
-                Score 7: The answer aligns with the reference but has minor omissions.
-                Score 10: The answer is completely accurate and aligns perfectly with the reference.
+                Score 2: The answer has minor relevance but does not align with the reference.
+                Score 3: The answer has moderate relevance but contains inaccuracies.
+                Score 4: The answer aligns with the reference but has minor omissions.
+                Score 5: The answer is completely accurate and aligns perfectly with the reference.
                 Only respond with a numberical score"""}
     SYSTEM_MESSAGE = "You are a helpful assistant."
     def __init__(self,
@@ -38,15 +38,6 @@ class OpenAIEvaluator(Evaluator):
         self.true_answer = true_answer
         self.question_asked = question_asked
 
-        # api_key = os.getenv('NIAH_EVALUATOR_API_KEY')
-        # if (not api_key):
-        #     raise ValueError("NIAH_EVALUATOR_API_KEY must be in env for using openai evaluator.")
-
-        # self.api_key = api_key
-        # self.evaluator = ChatAnthropic(temperature=0, model="claude-3-5-sonnet-20240620")
-        # # self.evaluator = ChatOpenAI(model=self.model_name,
-        # #                             openai_api_key=self.api_key,
-        # #                             **self.model_kwargs)
 
     def evaluate_response(self, response: str) -> int:
         # print('true answer: ', self.true_answer)
@@ -61,20 +52,21 @@ class OpenAIEvaluator(Evaluator):
         openai_url = os.environ.get("OPENAI_URL", None)
         headers = {'api-key': openai_key, 'Content-Type': 'application/json'}
         url = openai_url
-        i = 3
-        while i > 0:
+        try:
+            verdict_response = requests.post(url=url, headers=headers, json=payload)
+            verdict = verdict_response.json()['choices'][0]['message']['content']
+            print(verdict)
+            score = int(re.findall(r"\[\s*\+?(-?\d+)\s*\]", verdict)[0])
+        except:
+            print(verdict_response.text)
+            print('gpt call failed, trying claude...')
+            anthropic_key = os.environ.get("ANTHROPIC_KEY", None)
+            client = anthropic.Anthropic(api_key=anthropic_key)
             try:
-                verdict_response = requests.post(url=url, headers=headers, json=payload)
-                verdict = verdict_response.json()['choices'][0]['message']['content']
-                print(verdict)
-                score = int(re.findall(r"\[\s*\+?(-?\d+)\s*\]", verdict)[0])
-                i = 0
+                verdict = client.messages.create(model="claude-3-5-sonnet-20240620", system=self.SYSTEM_MESSAGE, messages=[{'role': 'user', 'content': "[Instruction]\nPlease act as an impartial judge and evaluate the quality of the response provided by an AI assistant to the user question displayed below. {criteria}\n[Ground truth]\n{reference}\nBegin your evaluation by providing a short explanation. Be as objective as possible. After providing your explanation, you must rate the response on a scale of 1 to 10 by strictly following this format: '[[rating]]', for example: 'Rating: [[5]]'.\n\n[Question]\n{input}\n\n[The Start of Assistant\'s Answer]\n{prediction}\n[The End of Assistant\'s Answer]".format(criteria=self.CRITERIA, reference=self.true_answer, input=self.question_asked, prediction=response)}], max_tokens=500).content[0].model_dump()['text']
             except:
-                i -= 1
+                print('claude failed too')
                 score = -1
-                time.sleep(10)
-                print(verdict_response.text)
-                print(f'gpt call failed {i}')
         
             
         print('done evaluating')
